b6279361b64f93be632e56b928af952317f0506c
BartoszJanota <bartosz.janota@gmail.com>
2015-04-08 18:30:41 +0200

    added graphic library

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import simpy
+import pygame
 
 from models.meadow import Meadow
 
 ENV_RUNNING_TIME = 100
 
-def main():
+class Main:
 
-    env = simpy.Environment()
-    meadow = Meadow(env)
-    env.run(until=ENV_RUNNING_TIME)
+    def __init__(self):
+        self.env = simpy.Environment()
+        self.meadow = Meadow(self.env)
+        self.env.run(until=ENV_RUNNING_TIME)
+        self.running = True
+        self.simulation_display = None
+
+    def init_pygame(self):
+        pygame.init()
+        self.simulation_display = pygame.display.set_mode((800,600))
+        pygame.display.set_caption('Wolf Hare Coevoultion Simulation')
+
+    def simulation(self):
+        self.init_pygame()
+        self.start_rendering()
+
+    def start_rendering(self):
+        clock = pygame.time.Clock()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+                print(event)
+
+            pygame.display.update()
+            clock.tick(60)
+        self.stop_rendering()
+
+    def stop_rendering(self):
+        pygame.quit()
+
 
 if __name__ == "__main__":
-    main()
+    main = Main()
+    main.simulation()
